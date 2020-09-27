c1a5b866d9805f88a314b08e4ca09c638b3037b6
Sahab Singh <sahabsinghgurjar@yahoo.com>
2020-09-27 12:50:48 -0500

    functional composition stream

--- src/main/java/com/sahab/order/stream/config/KafkaStreamConfig.java
+++ src/main/java/com/sahab/order/stream/config/KafkaStreamConfig.java
@@ -1,10 +1,32 @@
 package com.sahab.order.stream.config;
 
+import java.util.function.Function;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.sahab.order.common.model.OrderDetails;
 
 @Configuration
 public class KafkaStreamConfig {
+	
+	@Bean
+	public Function<OrderDetails,OrderDetails> processOrder(){
+		return (input)->{
+			System.out.println("Message received in transformer0 "+input);
+			input.setOrderName("transformed");
+			return input;
+		};
+	}
 
+	@Bean
+	public Function<OrderDetails,OrderDetails> processOrder1(){
+		return (input)->{
+			System.out.println("Message received in transformer1 "+input);
+			input.setOrderName("transformed");
+			return input;
+		};
+	}
 
 
 }
