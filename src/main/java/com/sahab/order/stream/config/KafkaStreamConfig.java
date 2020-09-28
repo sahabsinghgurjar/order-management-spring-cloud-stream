864ef2a7688469e798cddf11576023478a44f27f
Sahab Singh <sahabsinghgurjar@yahoo.com>
2020-09-27 19:18:56 -0500

    multi out stream implemented

--- src/main/java/com/sahab/order/stream/config/KafkaStreamConfig.java
+++ src/main/java/com/sahab/order/stream/config/KafkaStreamConfig.java
@@ -1,7 +1,11 @@
 package com.sahab.order.stream.config;
 
+import java.util.Arrays;
 import java.util.function.Function;
 
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Predicate;
+import org.apache.kafka.streams.kstream.ValueMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,23 +14,32 @@ import com.sahab.order.common.model.OrderDetails;
 @Configuration
 public class KafkaStreamConfig {
 	
-	@Bean
-	public Function<OrderDetails,OrderDetails> processOrder(){
-		return (input)->{
-			System.out.println("Message received in transformer0 "+input);
-			input.setOrderName("transformed");
-			return input;
-		};
-	}
+	
 
+	@SuppressWarnings("unchecked")
 	@Bean
-	public Function<OrderDetails,OrderDetails> processOrder1(){
-		return (input)->{
-			System.out.println("Message received in transformer1 "+input);
-			input.setOrderName("transformed");
-			return input;
-		};
+	public Function<KStream<Object, OrderDetails>, KStream<?, OrderDetails>[]> processOrder() {
+
+	    Predicate<Object, OrderDetails> isElectronics = (k, v) -> v.getOrderName().endsWith("ELECTRONICS");
+	    Predicate<Object, OrderDetails> isGrocerry = (k, v) -> !v.getOrderName().endsWith("ELECTRONICS");
+	    return (KStream<Object, OrderDetails> input)->
+	    {
+	    	return input.mapValues(new ValueMapper<OrderDetails, OrderDetails>() {
+
+				@Override
+				public OrderDetails apply(OrderDetails value) {
+					System.out.println(value);
+					return value;
+				}
+			})
+	            .flatMapValues(value ->{ 
+	            	return Arrays.asList(value);
+	            	
+	            })
+	            .branch(isElectronics, isGrocerry);
+	    };
 	}
+	
 
 
 }
